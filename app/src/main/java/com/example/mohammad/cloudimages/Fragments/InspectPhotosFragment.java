3c29be92f1ad840af5df108139f85002b92291ff
Espfish <3dt@live.com>
2018-01-28 20:32:38 +0200

    First Commit

--- app/src/main/java/com/example/mohammad/cloudimages/Fragments/InspectPhotosFragment.java
+++ app/src/main/java/com/example/mohammad/cloudimages/Fragments/InspectPhotosFragment.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.example.mohammad.cloudimages.R;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.googlecode.flickrjandroid.Flickr;
 import com.googlecode.flickrjandroid.FlickrException;
 import com.googlecode.flickrjandroid.photos.Photo;
@@ -19,15 +22,18 @@ import com.googlecode.flickrjandroid.photos.PhotosInterface;
 import com.googlecode.flickrjandroid.photos.SearchParameters;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InspectPhotosFragment extends Fragment {
 
-    private String api_key = "499f028356af62d51ef2e641df830fcf";
+    private String api_key;
+
     public InspectPhotosFragment() {
         // Required empty public constructor
     }
@@ -37,6 +43,13 @@ public class InspectPhotosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        try {
+            api_key = getApiKey();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         View rootView = inflater.inflate(R.layout.fragment_inspect_photos, container, false);
         EditText searchText = rootView.findViewById(R.id.edit_search);
         searchText.setText("Love");
@@ -51,7 +64,7 @@ public class InspectPhotosFragment extends Fragment {
             public void run() {
                 PhotoList photos;
                 try {
-                    photos = photoInterface.search(searchParameters,10,0);
+                    photos = photoInterface.search(searchParameters, 10, 0);
                     Photo photo = photos.get(0);
                     String photoURL = photo.getUrl();
                     Log.d("PHOTO_URL", photoURL);
@@ -64,4 +77,17 @@ public class InspectPhotosFragment extends Fragment {
         return rootView;
     }
 
+    public String getApiKey() throws IOException, JSONException {
+        String apiKey;
+        InputStream inputStream = getActivity().getAssets().open("api_keys.json");
+        int size = inputStream.available();
+        byte[] buffer = new byte[size];
+        inputStream.read(buffer);
+        inputStream.close();
+        String json = new String(buffer, "UTF-8");
+        Gson gson = new Gson();
+        JSONObject jObject = new JSONObject(json);
+        apiKey = jObject.getString("flickr_api_key");
+        return apiKey;
+    }
 }
